59c71baf4e1247fbec5a9022277d2e982a47ed96
Jayabal <m.jayabal@cmlmediasoft.com>
2018-09-10 22:47:12 +0530

    Setup startRemoveExpenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -31,6 +31,15 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ( {id} = {} ) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(startRemoveExpense({ id }));
+      // console.log({id});
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, removeExpense } from '../actions/expenses';
+import { editExpense, startRemoveExpense } from '../actions/expenses';
 
 class EditExpense extends React.Component {
 
@@ -11,7 +11,7 @@ class EditExpense extends React.Component {
     }
 
     onClick = () => {
-        this.props.removeExpense({id:this.props.expense.id});
+        this.props.startRemoveExpense({id:this.props.expense.id});
         this.props.history.push('/');
     }
 
@@ -41,7 +41,7 @@ const mapStatetoProps = (state, props) => {
 const mapDispatchToProps = (dispatch, props) => {
     return {
         editExpense: (id, expenses) => dispatch(editExpense(id, expenses)),
-        removeExpense: (data) => dispatch(removeExpense(data))
+        startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
     }
 }
 
